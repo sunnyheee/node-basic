42f5d1eef7dc1e7dfb22af79daddf85531018778
sunnyheee <fool0116@naver.com>
2024-08-18 19:05:57 +0900

    동기 비동기

---
+++ 6-express/_app.js
@@ -0,0 +1,74 @@
+import express from 'express'
+const app = express()
+
+// app.get('/', (req, res, nex) => {
+//   console.log(req.params)
+//   console.log(req.query)
+
+//   res.send('hi')
+// })
+// app.listen(8080)
+
+// app.get('/sky/:id', (req, res, nex) => {
+//   console.log(req.params)
+//   console.log(req.params.id)
+//   console.log(req.query)
+
+//   res.send('hi')
+// })
+// app.listen(8080)
+
+// app.get('/sky/:id', (req, res, nex) => {
+//   console.log(req.params)
+//   console.log(req.params.id)
+//   console.log(req.query)
+
+//   res.json({ id: 'test' })
+//   //   res.statusCode(404)
+//   res.setHeader('key', 'value')
+//   res.status(201).send('created')
+// })
+// app.listen(8080)
+
+// 미들웨어
+// app.all('/api', (req, res, next) => {
+//   // api만
+//   console.log('all')
+//   next()
+// })
+// app.use('/sky', (req, res, next) => {
+//   // sky를 포함한 모든, /sky/123 OK
+//   console.log('use')
+//   next()
+// })
+// app.get(
+//   '/',
+//   (req, res, next) => {
+//     console.log('first')
+//     // next()
+//     // next('route')
+//     next(new Error('error'))
+//   },
+//   (req, res, next) => {
+//     console.log('first2')
+//   },
+// ),
+//   app.get('/', (req, res, next) => {
+//     console.log('second')
+//   })
+// // 404페이지 처리
+// app.use((req, res, next) => {
+//   res.status(404).send('Not availble!')
+// })
+// app.use((error, req, res, next) => {
+//   console.error(error)
+//   res.status(500).send('Sorry, try later!')
+// })
+// app.listen(8080)
+
+// POST
+app.use(express.json())
+app.post('/', (res, req, next) => {
+  console.log(req.body)
+})
+app.listen(8080)
--- 6-express/app.js
+++ 6-express/app.js
@@ -1,74 +1,27 @@
+//동기 비동기처리
+
 import express from 'express'
+import fs from 'fs'
+import fsASync from 'fs/promises'
+
 const app = express()
-
-// app.get('/', (req, res, nex) => {
-//   console.log(req.params)
-//   console.log(req.query)
-
-//   res.send('hi')
-// })
-// app.listen(8080)
-
-// app.get('/sky/:id', (req, res, nex) => {
-//   console.log(req.params)
-//   console.log(req.params.id)
-//   console.log(req.query)
-
-//   res.send('hi')
-// })
-// app.listen(8080)
-
-// app.get('/sky/:id', (req, res, nex) => {
-//   console.log(req.params)
-//   console.log(req.params.id)
-//   console.log(req.query)
-
-//   res.json({ id: 'test' })
-//   //   res.statusCode(404)
-//   res.setHeader('key', 'value')
-//   res.status(201).send('created')
-// })
-// app.listen(8080)
-
-// 미들웨어
-// app.all('/api', (req, res, next) => {
-//   // api만
-//   console.log('all')
-//   next()
-// })
-// app.use('/sky', (req, res, next) => {
-//   // sky를 포함한 모든, /sky/123 OK
-//   console.log('use')
-//   next()
-// })
-// app.get(
-//   '/',
-//   (req, res, next) => {
-//     console.log('first')
-//     // next()
-//     // next('route')
-//     next(new Error('error'))
-//   },
-//   (req, res, next) => {
-//     console.log('first2')
-//   },
-// ),
-//   app.get('/', (req, res, next) => {
-//     console.log('second')
-//   })
-// // 404페이지 처리
-// app.use((req, res, next) => {
-//   res.status(404).send('Not availble!')
-// })
-// app.use((error, req, res, next) => {
-//   console.error(error)
-//   res.status(500).send('Sorry, try later!')
-// })
-// app.listen(8080)
-
-// POST
 app.use(express.json())
-app.post('/', (res, req, next) => {
-  console.log(req.body)
+
+app.get('/file', (res, req, next) => {
+  fs.readFile('/file1.txt', (err, data) => {
+    if (err) {
+      res.sendStatus(404)
+    }
+  })
 })
+
+app.get('/file1', (res, req, next) => {
+  try {
+    const data = fs.readFileSync('/file1.txt')
+    res.send(data)
+  } catch (error) {
+    res.sendStatus(404)
+  }
+})
+
 app.listen(8080)
