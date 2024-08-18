6e1d21803b080378367abf738c712af0f77398f4
sunnyheee <fool0116@naver.com>
2024-08-18 18:38:30 +0900

    res

--- 6-express/app.js
+++ 6-express/app.js
@@ -9,11 +9,23 @@ const app = express()
 // })
 // app.listen(8080)
 
+// app.get('/sky/:id', (req, res, nex) => {
+//   console.log(req.params)
+//   console.log(req.params.id)
+//   console.log(req.query)
+
+//   res.send('hi')
+// })
+// app.listen(8080)
+
 app.get('/sky/:id', (req, res, nex) => {
   console.log(req.params)
   console.log(req.params.id)
   console.log(req.query)
 
-  res.send('hi')
+  res.json({ id: 'test' })
+  //   res.statusCode(404)
+  res.setHeader('key', 'value')
+  res.status(201).send('created')
 })
 app.listen(8080)
