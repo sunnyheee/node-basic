426ee7f397a45059626c81f9aeef5fed653bd676
sunnyheee <fool0116@naver.com>
2024-08-18 18:49:54 +0900

    미들웨ㅐ어

--- 6-express/app.js
+++ 6-express/app.js
@@ -18,14 +18,50 @@ const app = express()
 // })
 // app.listen(8080)
 
-app.get('/sky/:id', (req, res, nex) => {
-  console.log(req.params)
-  console.log(req.params.id)
-  console.log(req.query)
+// app.get('/sky/:id', (req, res, nex) => {
+//   console.log(req.params)
+//   console.log(req.params.id)
+//   console.log(req.query)
 
-  res.json({ id: 'test' })
-  //   res.statusCode(404)
-  res.setHeader('key', 'value')
-  res.status(201).send('created')
+//   res.json({ id: 'test' })
+//   //   res.statusCode(404)
+//   res.setHeader('key', 'value')
+//   res.status(201).send('created')
+// })
+// app.listen(8080)
+
+// 미들웨어
+app.all('/api', (req, res, next) => {
+  // api만
+  console.log('all')
+  next()
+})
+app.use('/sky', (req, res, next) => {
+  // sky를 포함한 모든, /sky/123 OK
+  console.log('use')
+  next()
+})
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('first')
+    // next()
+    // next('route')
+    next(new Error('error'))
+  },
+  (req, res, next) => {
+    console.log('first2')
+  },
+),
+  app.get('/', (req, res, next) => {
+    console.log('second')
+  })
+// 404페이지 처리
+app.use((req, res, next) => {
+  res.status(404).send('Not availble!')
+})
+app.use((error, req, res, next) => {
+  console.error(error)
+  res.status(500).send('Sorry, try later!')
 })
 app.listen(8080)
